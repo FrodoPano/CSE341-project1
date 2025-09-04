dc4726b7e2d72cfc9d1ca5b2ec00e8873b6a9e2f
FrodoPano <frodopano@gmail.com>
2025-09-04 17:47:51 -0600

    try

--- data/database.js
+++ data/database.js
@@ -10,13 +10,20 @@ const initDb = (callback) => {
         console.log('Db is already initialized!');
         return callback(null, database);
     }
+    if (!process.env.MONGODB_URI) {
+        console.error('MONGODB_URI is not defined in environment variables');
+        return callback(new Error('MONGODB_URI is not defined'), null);
+    }
+
     MongoClient.connect(process.env.MONGODB_URI)
         .then((client) => {
             database = client;
-          callback(null, database);
+            console.log('Connected to MongoDB successfully');
+            callback(null, database);
         })
         .catch((err) => {
-          callback(err);
+            console.error('Failed to connect to MongoDB:', err);
+            callback(err);
         });
 };
 
